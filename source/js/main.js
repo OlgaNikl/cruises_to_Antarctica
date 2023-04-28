50b9ce08b8422cd21251079becc6b78340b899c1
OlgaNikl <hi.olganikl@gmail.com>
2023-04-28 17:47:59 +0300

    добавляет бургер меню, путешествия

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {Burger} from './modules/burger/burger';
 
 // ---------------------------------
 
@@ -17,6 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const burger = new Burger();
+    burger.init();
     initModals();
     const form = new Form();
     window.form = form;
